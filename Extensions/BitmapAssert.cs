7417807ff44ca6d08d9962a1de4eb4cbb22e1b38
rl132 <remilepine@hotmail.com>
2014-10-31 18:32:42 -0400

    Add XmlDocumentation for ColorExtension and BitmapAssert

--- Extensions/BitmapAssert.cs
+++ Extensions/BitmapAssert.cs
@@ -6,34 +6,77 @@ using RLToolkit.Extensions;
 
 namespace RLToolkit.Extensions
 {
+    /// <summary>
+    /// Class to extends the functionalities of nUnit by adding support for comparing Bitmaps
+    /// </summary>
     public static class BitmapAssert
     {
         #region AreEqual
+        /// <summary>
+        /// Are equal.
+        /// </summary>
+        /// <param name="source">Source.</param>
+        /// <param name="actual">Actual.</param>
+        /// <param name="message">Message.</param>
         public static void AreEqual(Bitmap source, Bitmap actual, string message)
         {
             AreEqual(source, actual, 0, true, message);
         }
 
+        /// <summary>
+        /// Are equal.
+        /// </summary>
+        /// <param name="source">Source.</param>
+        /// <param name="actual">Actual.</param>
+        /// <param name="alphaSupport">If set to <c>true</c> alpha support.</param>
         public static void AreEqual(Bitmap source, Bitmap actual, bool alphaSupport)
         {
             AreEqual(source, actual, 0, true, "");
         }
 
+        /// <summary>
+        /// Are equal.
+        /// </summary>
+        /// <param name="source">Source.</param>
+        /// <param name="actual">Actual.</param>
+        /// <param name="tolerence">Tolerence.</param>
         public static void AreEqual(Bitmap source, Bitmap actual, int tolerence)
         {
             AreEqual(source, actual, tolerence, true, "");
         }
 
+        /// <summary>
+        /// Are equal.
+        /// </summary>
+        /// <param name="source">Source.</param>
+        /// <param name="actual">Actual.</param>
+        /// <param name="tolerence">Tolerence.</param>
+        /// <param name="alphaSupport">If set to <c>true</c> alpha support.</param>
         public static void AreEqual(Bitmap source, Bitmap actual, int tolerence, bool alphaSupport)
         {
             AreEqual(source, actual, tolerence, alphaSupport, "");
         }
 
+        /// <summary>
+        /// Are equal.
+        /// </summary>
+        /// <param name="source">Source.</param>
+        /// <param name="actual">Actual.</param>
+        /// <param name="tolerence">Tolerence.</param>
+        /// <param name="message">Message.</param>
         public static void AreEqual(Bitmap source, Bitmap actual, int tolerence, string message)
         {
             AreEqual(source, actual, tolerence, true, message);
         }
 
+        /// <summary>
+        /// Are equal
+        /// </summary>
+        /// <param name="source">Source.</param>
+        /// <param name="actual">Actual.</param>
+        /// <param name="tolerence">Tolerence.</param>
+        /// <param name="alphaSupport">If set to <c>true</c> alpha support.</param>
+        /// <param name="message">Message.</param>
         public static void AreEqual(Bitmap source, Bitmap actual, int tolerence, bool alphaSupport, string message)
         {
             Test(true, source, actual, tolerence, alphaSupport, message);
@@ -41,31 +84,71 @@ namespace RLToolkit.Extensions
         #endregion
 
         #region AreNotEqual
+        /// <summary>
+        /// Are not equal
+        /// </summary>
+        /// <param name="source">Source.</param>
+        /// <param name="actual">Actual.</param>
+        /// <param name="message">Message.</param>
         public static void AreNotEqual(Bitmap source, Bitmap actual, string message)
         {
             AreNotEqual(source, actual, 0, true, message);
         }
 
+        /// <summary>
+        /// Are not equal
+        /// </summary>
+        /// <param name="source">Source.</param>
+        /// <param name="actual">Actual.</param>
+        /// <param name="alphaSupport">If set to <c>true</c> alpha support.</param>
         public static void AreNotEqual(Bitmap source, Bitmap actual, bool alphaSupport)
         {
             AreNotEqual(source, actual, 0, true, "");
         }
 
+        /// <summary>
+        /// Are not equal
+        /// </summary>
+        /// <param name="source">Source.</param>
+        /// <param name="actual">Actual.</param>
+        /// <param name="tolerence">Tolerence.</param>
         public static void AreNotEqual(Bitmap source, Bitmap actual, int tolerence)
         {
             AreNotEqual(source, actual, tolerence, true, "");
         }
 
+        /// <summary>
+        /// Are not equal
+        /// </summary>
+        /// <param name="source">Source.</param>
+        /// <param name="actual">Actual.</param>
+        /// <param name="tolerence">Tolerence.</param>
+        /// <param name="alphaSupport">If set to <c>true</c> alpha support.</param>
         public static void AreNotEqual(Bitmap source, Bitmap actual, int tolerence, bool alphaSupport)
         {
             AreNotEqual(source, actual, tolerence, alphaSupport, "");
         }
 
+        /// <summary>
+        /// Are not equal
+        /// </summary>
+        /// <param name="source">Source.</param>
+        /// <param name="actual">Actual.</param>
+        /// <param name="tolerence">Tolerence.</param>
+        /// <param name="message">Message.</param>
         public static void AreNotEqual(Bitmap source, Bitmap actual, int tolerence, string message)
         {
             AreNotEqual(source, actual, tolerence, true, message);
         }
 
+        /// <summary>
+        /// Are not equal
+        /// </summary>
+        /// <param name="source">Source.</param>
+        /// <param name="actual">Actual.</param>
+        /// <param name="tolerence">Tolerence.</param>
+        /// <param name="alphaSupport">If set to <c>true</c> alpha support.</param>
+        /// <param name="message">Message.</param>
         public static void AreNotEqual(Bitmap source, Bitmap actual, int tolerence, bool alphaSupport, string message)
         {
             Test(false, source, actual, tolerence, alphaSupport, message);
@@ -73,6 +156,15 @@ namespace RLToolkit.Extensions
         #endregion
 
         #region Test
+        /// <summary>
+        /// Method to test if two bitmaps are equals with a tolerence support
+        /// </summary>
+        /// <param name="expectEquals">If set to <c>true</c> expect equals.</param>
+        /// <param name="source">Source.</param>
+        /// <param name="actual">Actual.</param>
+        /// <param name="tolerence">Tolerence.</param>
+        /// <param name="alphaSupport">If set to <c>true</c> alpha support.</param>
+        /// <param name="message">Message.</param>
         private static void Test(bool expectEquals, Bitmap source, Bitmap actual, int tolerence, bool alphaSupport, string message)
         {
             LogManager.Instance.Log().Debug(String.Format("Comparing bitmaps with tolerence {0} and alphaSupport: {1}", tolerence, alphaSupport));
@@ -147,4 +239,3 @@ namespace RLToolkit.Extensions
         #endregion
     }
 }
-
--- Extensions/ColorExtension.cs
+++ Extensions/ColorExtension.cs
@@ -3,23 +3,54 @@ using System.Drawing;
 
 namespace RLToolkit.Extensions
 {
+    /// <summary>
+    /// Class to extends the functionalities of the Color class
+    /// </summary>
     public static class ColorExtension
     {
+        /// <summary>
+        /// Compares a color.
+        /// </summary>
+        /// <returns><c>true</c>, if color are similqr, <c>false</c> otherwise.</returns>
+        /// <param name="c">Current color</param>
+        /// <param name="toCompare">To compare against.</param>
         public static bool CompareColor(this Color c, Color toCompare)
         {
             return CompareColor(c, toCompare, 0, true);
         }
 
+        /// <summary>
+        /// Compares a color.
+        /// </summary>
+        /// <returns><c>true</c>, if color are similqr, <c>false</c> otherwise.</returns>
+        /// <param name="c">Current color</param>
+        /// <param name="toCompare">To compare against.</param>
+        /// <param name="tolerance">Tolerance.</param>
         public static bool CompareColor(this Color c, Color toCompare, int tolerance)
         {
             return CompareColor(c, toCompare, tolerance, true);
         }
 
+        /// <summary>
+        /// Compares a color.
+        /// </summary>
+        /// <returns><c>true</c>, if color are similqr, <c>false</c> otherwise.</returns>
+        /// <param name="c">Current color</param>
+        /// <param name="toCompare">To compare against.</param>
+        /// <param name="alphaSupport">If set to <c>true</c> alpha support.</param>
         public static bool CompareColor(this Color c, Color toCompare, bool alphaSupport)
         {
             return CompareColor(c, toCompare, 0, alphaSupport);
         }
 
+        /// <summary>
+        /// Compares a color.
+        /// </summary>
+        /// <returns><c>true</c>, if color are similqr, <c>false</c> otherwise.</returns>
+        /// <param name="c">Current color</param>
+        /// <param name="toCompare">To compare against.</param>
+        /// <param name="tolerance">Tolerance.</param>
+        /// <param name="alphaSupport">If set to <c>true</c> alpha support.</param>
         public static bool CompareColor(this Color c, Color toCompare, int tolerance, bool alphaSupport)
         {
             if (!ComparePixelChannel(c.R, toCompare.R, tolerance))
@@ -44,10 +75,16 @@ namespace RLToolkit.Extensions
             return true;
         }
 
+        /// <summary>
+        /// Compare a pixel byte data against another, with a tolerance
+        /// </summary>
+        /// <returns><c>true</c>, if pixel byte is within the tolerance range, <c>false</c> otherwise.</returns>
+        /// <param name="source">Source pixel channel byte</param>
+        /// <param name="source"pixel channel byte to compare against</param>
+        /// <param name="tolerance">Tolerance.</param>
         private static bool ComparePixelChannel(byte source, byte actual, int tolerance)
         {
             return (Math.Abs(source - actual) < tolerance);
         }
     }
 }
-
