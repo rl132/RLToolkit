a7ed7522ad33fe99d12dbb9a5af7b49ce3dedb28
rl132 <remilepine@hotmail.com>
2014-10-31 12:38:16 -0400

    Adding the Xml Documentation to the MD5Helper module

--- BasicModules/MD5Helper/MD5Helper.cs
+++ BasicModules/MD5Helper/MD5Helper.cs
@@ -6,12 +6,23 @@ namespace RLToolkit.Basic
 {
     public static class MD5Helper
     {
+        /// <summary>
+        /// Implicit method that will call the ComputeHash method with lower case
+        /// </summary>
+        /// <returns>The hash</returns>
+        /// <param name="input">the input string</param>
         public static string ComputeHash(string input)
         {
             LogManager.Instance.Log().Debug("Computing implicit hash for:" + Environment.NewLine + input);
             return ComputeHash(input, false);
         }
 
+        /// <summary>
+        /// Method that will compute a MD5 hash.
+        /// </summary>
+        /// <returns>The hash</returns>
+        /// <param name="input">the input string</param>
+        /// <param name="upperCase">If set to <c>true</c> the hash will be all upper case.</param>
         public static string ComputeHash(string input, bool upperCase)
         {
             string casing = upperCase ? "uppercase" : "lowercase";
@@ -31,7 +42,13 @@ namespace RLToolkit.Basic
             }
             return sBuilder.ToString();
         }
-        
+
+        /// <summary>
+        /// Method that validate that the input string computes as the reference hash
+        /// </summary>
+        /// <returns><c>true</c>, if the input computes to was reference, <c>false</c> otherwise.</returns>
+        /// <param name="input">Input string</param>
+        /// <param name="reference">Reference hash</param>
         public static bool VerifyMd5Hash(string input, string reference)
         {
             LogManager.Instance.Log().Debug("verifying hash for:" + Environment.NewLine + input + Environment.NewLine + "Should be:" + Environment.NewLine + reference);
