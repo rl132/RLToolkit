c16cc7f1d6c9a33f8919a29bab158d4d12afcc5f
rl132 <remilepine@hotmail.com>
2014-12-02 12:33:18 -0500

    -Fix DynamicControls design -Removed unused reference in DynamicColumn -Added the DynamicRow widget -Fix some EOL

--- Widgets/DynamicRow.cs
+++ Widgets/DynamicRow.cs
@@ -9,57 +9,57 @@ using RLToolkit;
 namespace RLToolkit.Widgets
 {
     [System.ComponentModel.ToolboxItem(true)]
-    public partial class DynamicColumn : Gtk.Bin
+    public partial class DynamicRow : Gtk.Bin
     {
         private int numberControls = 1;
-		private System.Type baseControl;
+        private System.Type baseControl;
 		private object[] baseParam;
-		private Gtk.Widget[] arrayWidgets;
-		private int maxControls = 8;
+        private Gtk.Widget[] arrayWidgets;
+        private int maxControls = 8;
 
 		#region Constructors
-		public DynamicColumn ()
+		public DynamicRow ()
 		{
-			this.Log ().Debug ("Empty Constructor for the DynamicColumn");
+			this.Log ().Debug ("Empty Constructor for the DynamicRow");
 			this.Build ();
 
 			SetControlType (typeof(Gtk.Label), null);
 		}
 
-		public DynamicColumn (System.Type controlType)
+		public DynamicRow (System.Type controlType)
 		{
-			this.Log ().Debug ("Constructor for the DynamicColumn using " + controlType.Name);
+			this.Log ().Debug ("Constructor for the DynamicRow using " + controlType.Name);
 			this.Build ();
 
 			SetControlType (controlType, null);
 		}
 
-		public DynamicColumn (System.Type controlType, object[] param)
+		public DynamicRow (System.Type controlType, object[] param)
 		{
-			this.Log ().Debug ("Constructor for the DynamicColumn using " + controlType.Name + " with params");
+			this.Log ().Debug ("Constructor for the DynamicRow using " + controlType.Name + " with params");
 			this.Build ();
 
 			SetControlType (controlType, param);
 		}
 		#endregion
 
-		#region public methods
-		public void SetControlType(System.Type controlType, object[] param)
-		{
-			this.Log().Debug("Setting type to: " + controlType.Name);
+        #region public methods
+        public void SetControlType(System.Type controlType, object[] param)
+        {
+            this.Log().Debug("Setting type to: " + controlType.Name);
 
-			baseControl = controlType;
+            baseControl = controlType;
 			baseParam = param;
 
-			arrayWidgets = new Gtk.Widget[1];
+            arrayWidgets = new Gtk.Widget[1];
 			Gtk.Widget newWidget = CreateCtrl ();
 
-			arrayWidgets [0] = newWidget;
+            arrayWidgets [0] = newWidget;
 
-			RefreshControl ();
-		}
+            RefreshControl ();
+        }
 
-		public Gtk.Widget[] GetControlArray()
+        public Gtk.Widget[] GetControlArray()
         {
             this.Log().Debug("Fetching the control array");
             return arrayWidgets;
@@ -97,8 +97,8 @@ namespace RLToolkit.Widgets
         #endregion
 
         #region Helper methods
-		private Gtk.Widget CreateCtrl()
-		{
+        private Gtk.Widget CreateCtrl()
+        {
 			this.Log().Debug("Creating a new control");
 			if (baseParam == null) {
 				return (Gtk.Widget)Activator.CreateInstance (baseControl);
@@ -112,60 +112,60 @@ namespace RLToolkit.Widgets
             this.Log ().Debug ("Refreshing the controls");
 
             // wipe everything
-            foreach (Gtk.Widget ch in columnBox.Children) {
-                columnBox.Remove (ch);
+            foreach (Gtk.Widget ch in rowBox.Children) {
+                rowBox.Remove (ch);
             }
 
             // add the widgets
             int i = 0;
             foreach (Gtk.Widget w in arrayWidgets) {
-                columnBox.Spacing = i;
-                columnBox.Add (w);
+                rowBox.Spacing = i;
+                rowBox.Add (w);
                 i++;
             }
 
             // refresh all
-            columnBox.ShowAll ();
+            rowBox.ShowAll ();
         }
         #endregion
 
         #region events
         protected void OnBtnMinusClicked (object sender, EventArgs e)
         {
-            this.Log ().Info ("Trying to remove a control");
-            if (numberControls > 1) {
-                Gtk.Widget[] newArray = new Gtk.Widget[numberControls-1];
-                for (int i = 0; i<numberControls-1; i++) {
-                    newArray [i] = arrayWidgets [i];
-                }
-                arrayWidgets = newArray;
-                numberControls--;
+			this.Log ().Info ("Trying to remove a control");
+			if (numberControls > 1) {
+				Gtk.Widget[] newArray = new Gtk.Widget[numberControls-1];
+				for (int i = 0; i<numberControls-1; i++) {
+					newArray [i] = arrayWidgets [i];
+				}
+				arrayWidgets = newArray;
+				numberControls--;
 
-                RefreshControl ();
-            } else {
-                // i'm sorry dave but i cannot let you do this.
-                this.Log ().Warn ("Min number of control reached");
-            }
+				RefreshControl ();
+			} else {
+				// i'm sorry dave but i cannot let you do this.
+				this.Log ().Warn ("Min number of control reached");
+			}
         }
 
         protected void OnBtnPlusClicked (object sender, EventArgs e)
         {
-            this.Log ().Info ("Trying to add a control");
-            if (numberControls < maxControls) {
-                Gtk.Widget[] newArray = new Gtk.Widget[numberControls + 1];
-                for (int i = 0; i<numberControls; i++) {
-                    newArray [i] = arrayWidgets [i];
-                }
-                numberControls++;
-                arrayWidgets = newArray;
-                Gtk.Widget newControl = CreateCtrl();
-                arrayWidgets [numberControls - 1] = newControl;
+			this.Log ().Info ("Trying to add a control");
+			if (numberControls < maxControls) {
+				Gtk.Widget[] newArray = new Gtk.Widget[numberControls + 1];
+				for (int i = 0; i<numberControls; i++) {
+					newArray [i] = arrayWidgets [i];
+				}
+				numberControls++;
+				arrayWidgets = newArray;
+				Gtk.Widget newControl = CreateCtrl();
+				arrayWidgets [numberControls - 1] = newControl;
 
-                RefreshControl ();
-            } else {
-                // i'm sorry dave but i cannot let you do this.
-                this.Log ().Warn ("Max number of controls reached");
-            }
+				RefreshControl ();
+			} else {
+				// i'm sorry dave but i cannot let you do this.
+				this.Log ().Warn ("Max number of controls reached");
+			}
         }
         #endregion
     }
