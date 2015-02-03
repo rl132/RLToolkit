150fd351bdc353fe4b0f58489dad440c4363a670
rl132 <remilepine@hotmail.com>
2015-02-02 20:24:45 -0500

    Adding Assembly Info, strongly named key and update on projects to use them. Also adding a friend reference for the UnitTests project to use different assemblies.

---
+++ BasicModules/Properties/AssemblyInfoLinux.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("RLToolkit.BasicModules.Linux")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("R. Lepine")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.*")]
+
+[assembly: InternalsVisibleTo("RLToolkit.UnitTests.Linux, PublicKey=" +
+  "002400000480000094000000060200000024000052534131000400001" + 
+  "100000011856690b2fcb001d2cfe079c166c6fde415bdd03ae953d897" + 
+  "dd2a335a83736e4e1f2b96a00240d5ea9c0649626e418adc83f083c11" + 
+  "36e5f2cad286450639b0bca26cb67793607f3879fb4fe3e209b111abd" +
+  "f7f29eba0631b8dfa65953235aadb11f8835e467ab4f593e79c43ca0b" +
+  "75f7e290426af80386ef61354f4fc02bd94")]
+
